183b0fdfcb4505a836f7eeff429657724255a323
thunderstorm010 <yigitozdemir1299@gmail.com>
2024-06-21 20:30:12 +0300

    Add optional targetSystem and targetComponent fields

--- lib/mavlink_message.dart
+++ lib/mavlink_message.dart
@@ -9,6 +9,8 @@ abstract class MavlinkMessage {
 
   int get mavlinkMessageId;
   int get mavlinkCrcExtra;
+  int? get targetSystem => null;
+  int? get targetComponent => null;
 
   ByteData serialize();
 
